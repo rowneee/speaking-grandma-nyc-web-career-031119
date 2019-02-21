2142e5c698f9620af41a31b44f01848859a9b11e
Roni Shabo <roni.shabo@gmail.com>
2019-02-21 21:27:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,9 +12,9 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 if "Hi Grandma"
-  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 elsif "HI GRANDMA"
-  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 else 'I LOVE YOU GRANDMA!'
-  puts 'I LOVE YOU TOO PUMPKIN!'
+  return 'I LOVE YOU TOO PUMPKIN!'
 end
